d190fa3bc9c0d08b3d14151a6bee1d2cf302a831
Chris Kellett <iamchriskellett@gmail.com>
2024-10-09 19:20:34 +0100

    Re-imported the BasicTests unit tests.

---
+++ UnitTests/BasisTests.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutClassLibrary.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        private ICheckout _checkout;
+        private List<SKUItem>? _skuItems;
+        private List<SpecialPrice>? _specialPrices;
+
+        [SetUp]
+        public void Setup()
+        {
+            _checkout = new Checkout();
+            _skuItems = _checkout.GetSKUs();
+            _specialPrices = _checkout.GetSpecialPrices();
+        }
+
+        [Test]
+        public void EmptyCart()
+        {
+            _checkout.SessionComplete();
+            int total = _checkout.GetTotalPrice();
+            Assert.That(total, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void OneItem()
+        {
+            if (_skuItems != null)
+            {
+                foreach (SKUItem sku in _skuItems)
+                {
+                    _checkout.SessionComplete();
+                    _checkout.Scan(sku.SKU ?? "");
+                    int total = _checkout.GetTotalPrice();
+                    Assert.That(total, Is.EqualTo(sku.Price));
+                }
+            }
+            else
+            {
+                Assert.Fail("_skuItems was NULL");
+            }
+        }
+
+        [Test]
+        public void MultipleItems()
+        {
+            if (_skuItems != null)
+            {
+                int totalPrice = 0;
+                _checkout.SessionComplete();
+                foreach (SKUItem sku in _skuItems)
+                {
+                    _checkout.Scan(sku.SKU ?? "");
+                    totalPrice += sku.Price ?? 0;
+                }
+
+                int total = _checkout.GetTotalPrice();
+                Assert.That(total, Is.EqualTo(totalPrice));
+            }
+            else
+            {
+                Assert.Fail("_skuItems was NULL");
+            }
+        }
+
+        [Test]
+        public void OnlyInvalidItems()
+        {
+            if (_skuItems != null)
+            {
+                foreach (SKUItem sku in _skuItems)
+                {
+                    _checkout.SessionComplete();
+                    _checkout.Scan("X" + sku.SKU + "X" ?? "");
+                    int total = _checkout.GetTotalPrice();
+                    Assert.That(total, Is.EqualTo(0));
+                }
+            }
+            else
+            {
+                Assert.Fail("_skuItems was NULL");
+            }
+        }
+
+        [Test]
+        public void InvalidAndValidItems()
+        {
+            if (_skuItems != null)
+            {
+                foreach (SKUItem sku in _skuItems)
+                {
+                    _checkout.SessionComplete();
+                    _checkout.Scan(sku.SKU ?? "");
+                    _checkout.Scan("X" + sku.SKU + "X" ?? "");
+                    int total = _checkout.GetTotalPrice();
+                    Assert.That(total, Is.EqualTo(sku.Price));
+                }
+            }
+            else
+            {
+                Assert.Fail("_skuItems was NULL");
+            }
+        }
+    }
+}
--- UnitTests/obj/Debug/net6.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net6.0/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8da340e3564488872a30405b82e960304eb0422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07ced3aa21bbe6bf5764d9cc082aa4895d9b464")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

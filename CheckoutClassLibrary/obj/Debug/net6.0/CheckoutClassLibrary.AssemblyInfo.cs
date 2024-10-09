6de76682c79a85c530871ef138e6aa2c8dae281c
Chris Kellett <iamchriskellett@gmail.com>
2024-10-09 19:23:16 +0100

    Moved the .gitignore file following solution restructure

--- CheckoutClassLibrary/obj/Debug/net6.0/CheckoutClassLibrary.AssemblyInfo.cs
+++ CheckoutClassLibrary/obj/Debug/net6.0/CheckoutClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckoutClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c63adc9d91d6db6a840ab43b3a11aff812b4ff57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8bed74bf68fa5dc4b4123c1beb0ee1c52c79fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckoutClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckoutClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UnitTests/obj/Debug/net6.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net6.0/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d190fa3bc9c0d08b3d14151a6bee1d2cf302a831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8bed74bf68fa5dc4b4123c1beb0ee1c52c79fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
